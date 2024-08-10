68955b716e56835386a157f40e02bbe7a29c629d
Minhajur Rohoman <minhajurrohoman9016@gmail.com>
2024-08-10 17:26:01 +0600

    category changes

--- src/hooks/useCategories.tsx
+++ src/hooks/useCategories.tsx
@@ -6,27 +6,37 @@ import { useAppSelector } from "../redux/hooks";
 
 export const useCategories = () => {
     const  {categories} = useAppSelector(state => state.category)
-    // console.log(categories)
+    // console.log({categories})
     const isSubCategories = (category: any) => {
         const isSubCategory = 'sub_category' in category
         return isSubCategory
     };
+    const isBrand = (category: any) => {
+        const isBrand = 'brand' in category
+        return isBrand
+    };
     const categoryRender = (categories: string | any[]) => {
+        // console.log(categories)
         const allCategories: JSX.Element[] = [];
         for (let index = 0; index < categories.length; index += 1) {
             const category = categories[index]
             const isSubCategory = isSubCategories(category);
+            const brand = isBrand(category)
+            console.log(brand)
             allCategories.push(
                 <li key={category.id}>
                     <Link to={'#'}>
-                        {isSubCategory &&  <RiComputerFill className='text-2xl' />}
+                        {isSubCategory && <RiComputerFill className='text-2xl' />}
                         <span className='flex w-full items-center justify-between'>
                             <p>{category?.name}</p>
                             {isSubCategory && category?.sub_category?.length > 0 &&  <RiArrowRightSLine className='text-xl' />}
                         </span>
                     </Link>
-                    {categoryRender(category) && category?.sub_category?.length > 0 ? (
+                    {isSubCategories(category) && category?.sub_category?.length > 0 ? (
                         <ul className='py-3'>{categoryRender(category?.sub_category)}</ul>
+                    ) : null}
+                    {isBrand(category) && category?.brand?.length > 0 ? (
+                        <ul className='py-3'>{categoryRender(category?.brand)}</ul>
                     ) : null}
                 </li>
             )
